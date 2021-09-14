102d498416f147d69e66b49c17b05bc56a842198
VijitKumar27 <79002833+VijitKumar27@users.noreply.github.com>
2021-09-14 14:50:07 +0530

    Add files via upload

---
+++ xyz.js
@@ -0,0 +1,5 @@
+document.write("Thanks for visiting! Keep supporting us!</br> ");
+document.write("For details contact +91-8969896939 </br>mailid:hoteltaj@gmail.com</br>");
+var dt=new Date();
+//document.write(dt.getDay()+"/"+dt.getMonth()+"/"+dt.getFullYear ()+" "+dt.getHours()+":"+dt.getMinutes());
+document.write(dt.getDate() + "/" + dt.getMonth() + "/" + dt.getFullYear() + "         " + dt.getHours() + ":" + dt.getMinutes());
